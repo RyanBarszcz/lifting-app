acf86b7361ee90fc4816cc02d9d2f54bd4870c49
RyanBarszcz <113656726+RyanBarszcz@users.noreply.github.com>
2026-04-09 15:45:23 -0400

    fix types

--- types/index.ts
+++ types/index.ts
@@ -67,7 +67,6 @@ export interface WorkoutExercise {
 export interface WorkoutState {
     sessionId: string | null;
     startedAt: number | null;
-    completedAt: number | null;
     exercises: WorkoutExercise[];
     templateName?: string | null;
     title: string;
